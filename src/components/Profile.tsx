be9fd32c51d941827c3e6c4f7f229ef6267c98b8
lergnom <lergnom@mail.ru>
2021-04-28 17:39:26 +0300

    video #12 - App-Header-Portfolio components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://static.tildacdn.com/tild3466-6664-4830-b233-653431393864/photo.jpg" alt=""/>
+            </div>
+            <div>
+                ava +descriptopn
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+
+}
