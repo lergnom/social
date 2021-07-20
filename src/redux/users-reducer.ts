e6f0f8cff787c40ad89a929b94191e9e4ef13700
lergnom <lergnom@mail.ru>
2021-07-20 11:36:54 +0300

    modify users-reducer

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,91 @@
+import {stat} from "fs";
+import {DispatchProps, ProfilePageType} from "./store";
+
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+
+type UsersDispatchProps = DispatchFollowProps | DispatchUnFollowProps
+
+
+type DispatchFollowProps = {
+    type: 'FOLLOW'
+    userId: string
+}
+type DispatchUnFollowProps = {
+    type: 'UNFOLLOW'
+    userId: string
+}
+
+
+type LocationPropsType = {
+    city: string
+    country: string
+}
+
+type UsersPropsType = {
+    id: string
+    fullName: string
+    status: string
+    followed: boolean
+    location: LocationPropsType
+}
+
+type UserProps = {
+    users: Array<UsersPropsType>
+}
+
+
+const initialState: UserProps = {
+    users: [
+        {
+            id: '1',
+            followed: false,
+            fullName: 'Dmitriy',
+            status: 'I boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+        },
+        {
+            id: '2',
+            followed: true,
+            fullName: 'Zoe',
+            status: 'dark princess',
+            location: {city: 'Anapa', country: 'Russia'}
+        },
+        {id: '3', followed: true, fullName: 'Ivan', status: 'wild doom', location: {city: 'Kiev', country: 'Ukraine'}},
+    ]
+}
+
+const usersReducer = (state = initialState, action: UsersDispatchProps) => {
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state, users: state.users.map(user => {
+                    if (user.id === action.userId) {
+                        return {...user, followed: true}
+                    }
+                    return user
+                })
+            }
+        case UNFOLLOW:
+            return {
+                ...state, users: state.users.map(user => {
+                    if (user.id === action.userId) {
+                        return {...user, followed: false}
+                    }
+                    return user
+                })
+            }
+
+        default:
+            return state
+    }
+}
+
+export const followAC = (userId: string) => {
+    return {type: FOLLOW, userId} as const
+}
+
+export const unFollowAC = (userId: string) => ({type: UNFOLLOW, userId})
+
+
+
