0f03aafb88b52cdc5df8b696f0e1e42e8b81f2bc
lergnom <lergnom@mail.ru>
2021-07-20 14:25:55 +0300

    add types and callback for follow unfollow buttons

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import {UserProps} from "../../redux/users-reducer";
+import {UserProps, UsersPropsType} from "../../redux/users-reducer";
+import s from './users.module.css'
 
+type UserPropsTypes = {
+    users: Array<UsersPropsType>
+    follow: (id: string) => void
+    unfollow: (id: string) => void
+}
 
-export const Users = (props: UserProps) => {
+export const Users = (props: UserPropsTypes) => {
     return <>
         {
 
             props.users.map(user => <div key={user.id}>
-                <span> <div> <img/> </div><div> <button>Follow</button></div></span>
+                <span> <div> <img className={s.avatarWrapper} src={user.ava}/> </div><div> {user.followed ?
+                    <button onClick={() => {
+                        props.unfollow(user.id)
+
+                    }}>UnFollow</button> :
+                    <button onClick={() => {
+                        props.follow(user.id)
+                    }}>Follow</button>} </div></span>
                 <span>
                         <div>{user.fullName}</div>
                         <div>{user.status}</div>
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -28,8 +28,9 @@ type LocationPropsType = {
     country: string
 }
 
-type UsersPropsType = {
+export type UsersPropsType = {
     id: string
+    ava: string
     fullName: string
     status: string
     followed: boolean
@@ -45,6 +46,7 @@ const initialState: UserProps = {
     users: [
         {
             id: '1',
+            ava: 'https://drasler.ru/wp-content/uploads/2019/05/%D0%91%D0%B5%D0%BD%D0%B4%D0%B5%D1%80-%D1%84%D0%BE%D1%82%D0%BE-%D0%BD%D0%B0-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D1%83-%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-014.jpg',
             followed: false,
             fullName: 'Dmitriy',
             status: 'I boss',
@@ -52,12 +54,17 @@ const initialState: UserProps = {
         },
         {
             id: '2',
+            ava: 'https://drasler.ru/wp-content/uploads/2019/05/%D0%91%D0%B5%D0%BD%D0%B4%D0%B5%D1%80-%D1%84%D0%BE%D1%82%D0%BE-%D0%BD%D0%B0-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D1%83-%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-014.jpg',
             followed: true,
             fullName: 'Zoe',
             status: 'dark princess',
             location: {city: 'Anapa', country: 'Russia'}
         },
-        {id: '3', followed: true, fullName: 'Ivan', status: 'wild doom', location: {city: 'Kiev', country: 'Ukraine'}},
+        {
+            id: '3',
+            ava: 'https://drasler.ru/wp-content/uploads/2019/05/%D0%91%D0%B5%D0%BD%D0%B4%D0%B5%D1%80-%D1%84%D0%BE%D1%82%D0%BE-%D0%BD%D0%B0-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D1%83-%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-014.jpg',
+            followed: true, fullName: 'Ivan', status: 'wild doom', location: {city: 'Kiev', country: 'Ukraine'}
+        },
     ]
 }
 
