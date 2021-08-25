8f7e1fd88ee4f2f38944f1d251eb2b44edf798be
lergnom <lergnom@mail.ru>
2021-08-25 15:32:23 +0300

    video #75 - React JS - redux-form введение (login)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,43 @@
 import React from "react";
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
+
+type LoginFormDataType = {
+    login: string,
+    password: string,
+    remeberMe: boolean,
+}
+
+const LoginForm = (props: InjectedFormProps<LoginFormDataType>) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"login"} type="text" component={"input"} name={'login'}/>
+            </div>
+            <div>
+                <Field placeholder={"password"} type="password" component={"input"} name={'password'}/>
+            </div>
+            <label>
+                <Field component={"input"} name={'remeberMe'} type="checkbox"/> remeberMe
+            </label>
+            <div>
+                <button>LogIn</button>
+            </div>
+        </form>
+    )
+}
+const LoginReduxForm = reduxForm<LoginFormDataType>({
+    form: 'loginForm'
+})(LoginForm)
 
 export const Login = () => {
+    const onSubmit = (formData: LoginFormDataType) => {
+        console.log(formData.login);
+        console.log(formData.remeberMe);
+        console.log(formData)
+    }
     return <>
-        Login
+        <h1>Login Form</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
     </>
 
 }
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,19 +4,21 @@ import {dialogsReducer} from "./dialogs-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from 'redux-form';
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
 
 
-export let store = createStore(rootReducer,applyMiddleware(thunk))
+export let store = createStore(rootReducer, applyMiddleware(thunk))
 // @ts-ignore
 window.store = store
 
