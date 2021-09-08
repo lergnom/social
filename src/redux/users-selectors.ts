f170e791810497bab66e8ce64699456bbeb43b00
lergnom <lergnom@mail.ru>
2021-09-08 10:24:20 +0300

    video #83 - work with reselector

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,7 +1,13 @@
+import {createSelector} from "reselect";
+
 export const getUsersState = (state: any) => {
     return state.usersPage.users;
-
 };
+// реселектор позволяет не перирисовывать компоненты
+export const getUserSuper = createSelector(getUsersState, (users) => {
+    return users.filter();
+
+})
 
 export const getPageSize = (state: any) => {
     return state.usersPage.pageSize;
