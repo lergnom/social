9a10a28cdcd426182a25d9fbf48a64d5764b59ae
lergnom <lergnom@mail.ru>
2021-09-16 20:34:44 +0300

    #85 - React JS - hook, useEffect, хуки

--- src/components/Profile/ProfileStatus/ProfileStatusWithHook.tsx
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHook.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 type ProfileStatusType = {
@@ -9,6 +9,10 @@ type ProfileStatusType = {
 const ProfileStatusWithHook = (props: ProfileStatusType) => {
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const toggleStateStatus = (value: boolean) => {
         setEditMode(value);
