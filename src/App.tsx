d092f4acae6c3d3e8adb098a2470bb9b8471150d
lergnom <lergnom@mail.ru>
2021-11-10 09:19:18 +0300

    lazy and React.Suspens

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {UsersContainer} from './components/Users/UsersContainer';
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import {HeaderContainer} from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import {AppStateType} from "./redux/redux-store";
 import {Preloader} from "./common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+
 
 type AppComponentType = {
     initializeApp: () => void
@@ -21,27 +23,35 @@ type AppComponentType = {
 
 class App extends React.Component<AppComponentType> {
     componentDidMount() {
-        this.props.initializeApp()
+        this.props.initializeApp();
     }
 
     render() {
 
         if (!this.props.initialized) {
-            return <Preloader/>
+            return <Preloader/>;
         }
 
 
         return (
             <BrowserRouter>
-                <div className='app-wrapper'>
+                <div className="app-wrapper">
                     <HeaderContainer/>
                     {/*<Navbar/>*/}
-                    <div className='app-wrapper-content'>
+                    <div className="app-wrapper-content">
                         <Route path={"/dialogs"}
-                               render={() => <DialogsContainer/>
+                               render={() => {
+                                   return <React.Suspense fallback={<div>loading</div>}>
+                                       <DialogsContainer/>
+                                   </React.Suspense>;
+                               }
                                }/>
                         <Route path={"/profile/:userId?"}
-                               render={() => <ProfileContainer/>
+                               render={() => {
+                                   return <React.Suspense fallback={<div>loading</div>}>
+                                       <ProfileContainer/>
+                                   </React.Suspense>;
+                               }
                                }/>
                         <Route path={"/users"}
                                render={() => <UsersContainer/>}/>
@@ -60,8 +70,8 @@ class App extends React.Component<AppComponentType> {
 
 const mapStateToProps = (state: AppStateType) => ({
     initialized: state.app.initialized,
-})
+});
 
 export default connect(mapStateToProps, {
     initializeApp
-})(App)
+})(App);
